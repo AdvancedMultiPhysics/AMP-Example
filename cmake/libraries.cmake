ea204f025f9469a800d708c0d194dda566999fe6
Mark Berrill <berrillma@ornl.gov>
2024-05-30 08:53:46 -0400

    Fixing cmake warning

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -1,6 +1,7 @@
 # Macro to configure specific options for this project
 MACRO ( CONFIGURE_THIS )
     # Put user code here
+    NULL_USE( ENABLE_GCOV )
     SET( LINK_LIBRARIES ${LINK_LIBRARIES} ${AMP_LIBS} )
 ENDMACRO ()
 
