0f3b320aaa72b00e178a2b84f2116fa4224d4b62
Mark Berrill <berrillma@ornl.gov>
2024-12-16 08:31:56 -0500

    Updating script and test

--- src/print.h
+++ src/print.h
@@ -1,2 +1,10 @@
-// print hello world
-void print_hello_world();
+#include "print.h"
+#include "AMP/utils/AMP_MPI.h"
+
+#include <iostream>
+
+void print_hello_world()
+{
+    AMP::AMP_MPI comm( AMP_COMM_WORLD );
+    std::cout << "Rank " << comm.getRank() << ":  Hello world\n";
+}
